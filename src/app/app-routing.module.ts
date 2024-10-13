cf558ef98170265b8d630370645022a46931b512
Diego Braz <diegobraz@souunisuam.com.br>
2024-10-13 17:46:19 -0300

    att

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) },
+  { path: 'products', loadChildren: () => import('./home/products/products.module').then( m => m.ProductsPageModule) },
+
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
